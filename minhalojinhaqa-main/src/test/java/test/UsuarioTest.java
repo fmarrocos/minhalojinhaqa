c6243c498556104e8415e28beab8e907c443ce24
Felipe Marrocos <marrocos@gmail.com>
2023-08-26 14:23:58 -0300

    Teste Buscar Usuario por ID

--- minhalojinhaqa-main/src/test/java/test/UsuarioTest.java
+++ minhalojinhaqa-main/src/test/java/test/UsuarioTest.java
@@ -4,6 +4,8 @@ import com.github.javafaker.Faker;
 import entidade.Usuario;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -69,19 +71,31 @@ public class UsuarioTest {
         Faker faker = new Faker();
 
         Usuario usuario = new Usuario(
-                faker.name().fullName(), faker.internet().emailAddress(),
+                faker.name().fullName(),faker.internet().emailAddress(),
                 "teste123",
                 "true");
+
+        Response response = RestAssured
+                .given()
+                    .contentType(ContentType.JSON)
+                .when()
+                    .body(usuario)
+                    .post("usuarios")
+                .then()
+                    .statusCode(HttpStatus.SC_CREATED)
+                    .and().extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        String id = jsonPath.get("_id");
+        System.out.println(id);
 
         RestAssured
                 .given()
                     .contentType(ContentType.JSON)
-                    .log().all()
                 .when()
-                .get("usuarios/{_id}", "j3Bf13gp60KDXuLW")
+                    .get("usuarios/{_id}", id)
                 .then()
-                .statusCode(HttpStatus.SC_OK)
-                .log().all();
+                    .statusCode(HttpStatus.SC_OK)
+                    .log().all();
     }
 
 }
